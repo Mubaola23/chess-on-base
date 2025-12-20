63708fe4955a839d9d539abca4afb5d87e224516
mubaola23 <mubarakshuaib3@gmail.com>
2025-12-20 11:42:15 +0100

    update

--- web3-chess/src/app/page.tsx
+++ web3-chess/src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import { JoinGame } from '@/components/JoinGame';
+import { sdk } from '@farcaster/miniapp-sdk';
+import { useEffect } from 'react';
 
 export default function Home() {
+    useEffect(() => {
+        sdk.actions.ready();
+    }, []);
+
   return (
     <main>
       <JoinGame />
